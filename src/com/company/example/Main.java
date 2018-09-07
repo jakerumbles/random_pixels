96ac1a6807c36862e3e5b7db4dcd374f6ac36e5c
Jake Edwards <edwardsjake@live.com>
2018-09-07 13:15:44 -0600

    Randomize button works now
    The randomize button now works.  I implemented the click event for the randomize button.  Each time it is pressed a new random image is generated and stored in the images folder.  I added a counter so it keeps all of the images and simply adds a counter to the end of the name starting at 0.  They will be overwritten when the program is run again so deleting is not necessary although I might add it to the on close event just to be tidy.  The image path is now relative and should work on different computers.

--- src/com/company/example/Main.java
+++ src/com/company/example/Main.java
@@ -2,8 +2,9 @@ package com.company.example;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,22 +13,31 @@ public class Main {
 
     public static int HEIGHT = 1200;
     public static int WIDTH = 1200;
+    public static JFrame frame;
+    public static ImageIcon ii;
+    public static JLabel label;
+    public static int imageCounter = 0;
 
     public static void main(String[] args) throws IOException{
 
         //Create frame
-        JFrame frame = new JFrame("Random Pixels");
+        frame = new JFrame("Random Pixels");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
 
         createRandomImage();
 
-        ImageIcon ii = new ImageIcon("C:\\Users\\edwar\\Documents\\Software_Engineering\\random_pixels\\src\\com\\company\\example\\random_pixels.png");
-        JLabel label = new JLabel(ii);
+        String workingDir = System.getProperty("user.dir");
+        String imagePath = workingDir + "\\images\\random_pixels" + imageCounter + ".png";
+
+        ii = new ImageIcon(imagePath);
+        label = new JLabel(ii);
         JButton button = new JButton("Randomize!");
+
+
         frame.getContentPane().add(label, BorderLayout.CENTER);
         frame.getContentPane().add(button, BorderLayout.SOUTH);
+
+        button.addActionListener(new ClickEvent());
 
         //Size frame
         frame.setSize(WIDTH, HEIGHT);
@@ -36,6 +46,19 @@ public class Main {
         //Show frame
         frame.setVisible(true);
 
+    }
+
+    public static class ClickEvent implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            imageCounter++;
+            createRandomImage();
+            String workingDir = System.getProperty("user.dir");
+            String imagePath = workingDir + "\\images\\random_pixels" + imageCounter + ".png";
+            label.setIcon(new ImageIcon(imagePath));
+            frame.getContentPane().add(label, BorderLayout.CENTER);
+
+            System.out.println("Random clicked");
+        }
     }
 
     public static void createRandomImage() {
@@ -55,7 +78,12 @@ public class Main {
         }
 
         try {
-            f = new File("C:\\Users\\edwar\\Documents\\Software_Engineering\\random_pixels\\src\\com\\company\\example\\random_pixels.png");
+            String workingDir = System.getProperty("user.dir");
+            System.out.println(workingDir);
+            String imagePath = workingDir + "\\images\\random_pixels" + imageCounter + ".png";
+            System.out.println(imagePath);
+
+            f = new File(imagePath);
             ImageIO.write(img, "png", f);
         } catch (IOException e) {
             System.out.println("Error: " + e);
